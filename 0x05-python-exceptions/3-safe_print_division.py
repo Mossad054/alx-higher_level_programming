3349fa69f6cd7318ee1e95a851d590e74446a9a0
Mossad054 <michaelndungu054@gmail.com>
2024-02-06 13:15:59 -0800

    3safe

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+        return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    finally:
+        print("")
+        return result
