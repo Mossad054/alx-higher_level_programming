58e75c61de0e55f80e2f1c9325937d17fbbb1352
Mossad054 <michaelndungu054@gmail.com>
2024-02-06 10:54:17 -0800

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 def safe_print_list(my_list=[], x=0):
-    count = 0
     try:
+        count = 0
         for i in range(x):
-            print(my_list[i], end=' ')
+            print(my_list[i], end="")
             count += 1
     except IndexError:
         pass
-    print()
-    return count
+    finally:
+        print()
+        return count
