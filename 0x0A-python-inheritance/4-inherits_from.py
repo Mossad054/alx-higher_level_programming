964d70dc038776051871c2521028af3e9ac8485f
Mossad054 <michaelndungu054@gmail.com>
2024-02-21 13:45:10 -0800

    4is

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines an inherited class-checking function."""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is an inherited instance of a class.
+
+    Args:
+        obj (any):  object to check.
+        a_class (type): class to match the type of obj.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
