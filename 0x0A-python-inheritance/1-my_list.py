6d95696d0bb96666854e6975ad1bd97a5fd9141f
Mossad054 <michaelndungu054@gmail.com>
2024-02-21 13:23:43 -0800

    main1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
-
-
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
     def print_sorted(self):
-        """prints  sorted list"""
-        print(sorted(self))
+        """
+        Prints the list sorted in ascending order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
