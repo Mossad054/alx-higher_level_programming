85e3fe82e8c58e998c81699e10975db5985305d5
mossad054 <michaelndungu054@gmail.com>
2024-05-10 15:27:26 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Request to a URL and displays the response body
-  - Handles HTTP errors.
+""" script that;
+- takes in a URL
+- displays body of the response (decoded in utf-8).
 """
 import sys
 import urllib.error
@@ -10,10 +11,12 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
 
+## Make a GET request to the URL
     request = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("ascii"))
     except urllib.error.HTTPError as e:
+        # Handle HTTP errors and print the HTTP status.
         print("Error code: {}".format(e.code))
 
